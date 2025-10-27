0bd6799688ae4aea3b67d7b5929d429ac8ec4cf7
samiam1375 <smoral20@ford.com>
2025-10-26 23:01:02 -0400

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,51 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(item => {
+      const itemCost = parseFloat(item.cost.substring(1)); // Remove '$' and parse to float
+      total += itemCost * item.quantity;
+    });
+    return total.toFixed(2); // Format to 2 decimal places
   };
 
+  // This function is called when the "Continue Shopping" button is clicked
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault(); // Prevent default form submission or link navigation
+    onContinueShopping(); // Call the onContinueShopping function passed from the parent
   };
 
-
+  // Function for checkout shopping
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    // --- USAGE OF updateQuantity ACTION ---
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    // If the item's quantity is greater than 1, dispatch updateQuantity to decrease it
+    if (item.quantity > 1) {
+      // --- USAGE OF updateQuantity ACTION ---
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      // Else if the quantity would drop to 0, dispatch the removeItem action
+      // --- USAGE OF removeItem ACTION ---
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    // --- USAGE OF removeItem ACTION ---
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const itemCost = parseFloat(item.cost.substring(1)); // Remove '$' and parse to float
+    return (itemCost * item.quantity).toFixed(2); // Format to 2 decimal places
   };
 
   return (
@@ -55,14 +79,12 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <button className="get-started-button" onClick={handleContinueShopping}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
-
-
